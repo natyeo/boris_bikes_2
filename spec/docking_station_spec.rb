1ea1837911cf56f04c6213e74bad32e7e209c64b
Student <student@Makerss-MacBook-Pro.local>
2019-10-16 16:29:31 +0100

    wk1 (Boris Bike Challenge) 8 completed

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,4 +2,13 @@ require 'docking_station.rb'
 
 describe DockingStation do
 
+  # it "it responds to release_bike" do
+  #   station = DockingStation.new
+  #   expect(station.release_bike).to eq ()
+  # end
+
+  it "it responds to release_bike" do
+    expect(DockingStation.new).to respond_to :release_bike
+  end
+
 end
