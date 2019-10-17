d0ecb77bf19ddaf6f4658c76b9ed2247260bc385
Student <student@Makerss-MacBook-Pro.local>
2019-10-17 15:54:16 +0100

    completed step 11

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,17 @@
 class DockingStation
 
+ attr_reader :docked_bikes
+
   def release_bike
     Bike.new
+  end
+
+  def dock(bike)
+    @docked_bikes = bike
+  end
+
+  def show_docked_bikes
+    @docked_bikes
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -18,5 +18,20 @@ describe DockingStation do
     expect(bike).to be_working
   end
 
+  it { is_expected.to respond_to(:dock).with(1).argument }
+
+  it 'to dock a bike' do
+    bike1 = Bike.new
+    subject.dock(bike1)
+    expect(subject.dock(bike1)).to eq bike1
+  end
+
+  it { is_expected.to respond_to :show_docked_bikes }
+
+  it 'shows docked bikes' do
+    bike2 = Bike.new
+    subject.dock(bike2)
+    expect(subject.show_docked_bikes).to eq bike2
+  end
 
 end
